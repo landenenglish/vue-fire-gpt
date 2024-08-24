fd1d013bc1caa2aea2f2f358c9345b8e352d0afd
Landen English <englishlanden@gmail.com>
2024-08-24 15:47:49 -0400

    Prime tailwind setup

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-//
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 </script>
 
 <template>
   <div class="wrapper">
     <div class="underline text-3xl">Root route</div>
+    <DataTable table-style="min-width: 50rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
   </div>
 </template>
-
-<style lang="scss">
-.wrapper {
-  .inner {
-    font-size: 2rem;
-  }
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import primeui from 'tailwindcss-primeui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [primeui],
 }
