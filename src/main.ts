9a176b5ec97bf54dc456fb6eb7fcfe0a62afc076
Landen English <englishlanden@gmail.com>
2024-08-24 14:59:26 -0400

    PrimeVue, src imports

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { router } from './router'
+import { router } from '@/router'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(PrimeVue, { unstyled: true })
 
 app.mount('#app')
