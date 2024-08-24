30cb544d40470269b826a0fb870200a853f77dbf
Landen English <englishlanden@gmail.com>
2024-08-24 12:28:22 -0400

    progress setting up auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import path from 'path'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     //
     VueRouter(),
     vue(),
+    Components({
+      dts: true,
+      extensions: ['vue', 'ts'],
+      dirs: ['src/components'],
+    }),
+    AutoImport({
+      dts: true,
+      imports: ['vue', VueRouterAutoImports],
+      dirs: ['src/composables', 'src/constants', 'src/helpers'],
+    }),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
